052c18c3677a712ec4786daa753faea329629b14
epask <ecpaskey@iastate.edu>
2022-02-26 13:06:19 -0600

    integrates roy's code functionalities

--- cloud_flask_app/app/user_views.py
+++ cloud_flask_app/app/user_views.py
@@ -1,17 +1,18 @@
 import os
 import boto3
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, Response
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = "uploads"
+# UPLOAD_FOLDER = "uploads"
 BUCKET = "photos422"
 
 
 @app.route("/user/dashboard")
 def user_dashboard():
     # show_image retrieves a list of temporary, public image urls
-    contents = show_image(BUCKET)
+    # contents = show_image(BUCKET)
+    contents = get_bucket_files(BUCKET)
     # contents is passed to the dashboard.html file
     return render_template("user/dashboard.html", contents=contents)
 
@@ -19,25 +20,60 @@ def user_dashboard():
 def upload():
     if request.method == "POST":
         f = request.files['file']
-        f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
-        upload_file(f"uploads/{f.filename}", BUCKET)
-        return redirect("/user/dashboard")
+        resource = boto3.resource('s3')
+        bucket = resource.Bucket('photos422')
+        bucket.Object(f.filename).put(Body=f)
+    
+    return redirect("/user/dashboard")
+
+@app.route("/download", methods=['POST'])
+def download():
+    obj_key = request.form['key']
+    resource = boto3.resource('s3')
+    bucket = resource.Bucket('photos422')
+    file_obj = bucket.Object(obj_key).get()
+
+    resp = Response(file_obj['Body'].read())
+    resp.headers['Content-Disposition'] = 'attachment;filename=' + format(obj_key)
+    resp.mimetype = 'text/plain'
+
+    return resp
+
+@app.route('/search', methods=['GET'])
+def search():
+    file_name = request.args.get("search")
+    resource = boto3.resource('s3')
+    bucket = resource.Bucket('photos422')
+    object_list = bucket.objects.filter(Prefix=file_name)
+
+    return render_template('user/dashboard.html', contents=object_list)
 
 def show_image(bucket):
     s3_client = boto3.client('s3')
-    public_urls = []
+    # public_urls = []
+    files = []
     try:
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
-            presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': item['Key']}, ExpiresIn = 100)
-            public_urls.append(presigned_url)
-            print("URL: " + presigned_url)
+            # presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': item['Key']}, ExpiresIn = 100)
+            # tuple = (presigned_url, item['Key'])
+            # public_urls.append(tuple)
+            # print(tuple)
+            files.append(item['Key'])
 
     except Exception as e:
+        print(e)
         pass
-    return public_urls
+    return files
 
 def upload_file(file_name, bucket):
     object_name = file_name
     s3_client = boto3.client('s3')
     response = s3_client.upload_file(file_name, bucket, object_name)
     return response
+
+def get_bucket_files(bucket):
+    resource = boto3.resource('s3')
+    bucket = resource.Bucket('photos422')
+    object_list = bucket.objects.all()
+
+    return object_list
--- cloud_flask_app/app/views.py
+++ cloud_flask_app/app/views.py
@@ -1,10 +1,34 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import pymysql
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello world!"
-
-@app.route("/about")
-def about():
+    print("here")	
+    conn = pymysql.connect(
+            host= 'database-1.czglra39ojmn.us-east-1.rds.amazonaws.com', 
+            port = 3306,
+            user = 'admin', 
+            password = 'password',
+            db = 'db',
+            )
+    with conn:
+            with conn.cursor() as cursor:
+                print("conn")
+                if request.method == 'POST':
+                    username = request.form['username']
+                    password = request.form['password']
+                    cur=conn.cursor()
+                    query = "SELECT password from users Where username = '" + str(username) + "'"
+                    print(query)
+                    cur.execute(query)
+                    output = cur.fetchall()
+                    if password == output[0][0]:
+                        print("check made")
+                        return redirect("/user/dashboard")    
+    
     return render_template("public/index.html")
+
+# @app.route("/about")
+# def about():
+#     return render_template("public/index.html")
