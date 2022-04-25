a2fd7b16ce38f4709f6cf74b76b92310cb5a8d18
David Imhoff <dbimhoff@iastate.edu>
2022-04-25 16:55:39 -0500

    removed image stuff

--- cloud_flask_app/app/user_views.py
+++ cloud_flask_app/app/user_views.py
@@ -14,10 +14,6 @@ contents = None
 
 @app.route("/")
 def user_dashboard():
-    # show_image retrieves a list of temporary, public image urls
-    # contents = show_image(BUCKET)
-    global contents
-    # contents = get_bucket_files(environ.get('USERNAME'))
     return render_template("public/index.html")
 
 @app.route("/upload", methods=['POST'])
@@ -49,60 +45,6 @@ def download():
     resp.mimetype = 'text/plain'
 
     return resp
-
-@app.route('/search', methods=['GET'])
-def search():
-    file_name = request.args.get("search")
-    resource = boto3.resource('s3')
-    bucket = resource.Bucket(BUCKET)
-    # object_list = bucket.objects.filter(Prefix=file_name)
-    object_list = []
-    global contents
-    for word in contents:
-        if(file_name in word.key):
-            object_list.append(word)
-
-    return render_template('/', contents=object_list)
-
-def show_image(bucket):
-    s3_client = boto3.client('s3')
-    # public_urls = []
-    files = []
-    try:
-        for item in s3_client.list_objects(Bucket=bucket)['Contents']:
-            # presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': item['Key']}, ExpiresIn = 100)
-            # tuple = (presigned_url, item['Key'])
-            # public_urls.append(tuple)
-            # print(tuple)
-            files.append(item['Key'])
-
-    except Exception as e:
-        print(e)
-        pass
-    return files
-
-def upload_file(file_name, bucket):
-    object_name = file_name
-    s3_client = boto3.client('s3')
-    response = s3_client.upload_file(file_name, bucket, object_name)
-    return response
-
-def get_bucket_files(username):
-    resource = boto3.resource('s3')
-    bucket = resource.Bucket(BUCKET)
-    allImages = bucket.objects.all()
-    toRet = []
-    dynamodb = boto3.resource('dynamodb',aws_access_key_id=S3_KEY,aws_secret_access_key=S3_SECRET,region_name="us-east-1")
-    table = dynamodb.Table('Images')
-    userImages = table.query(KeyConditionExpression=Key('UserName').eq(username))
-
-    for i in userImages['Items']:
-        for image in allImages:
-            print(i['ImageName'])
-            if(i['ImageName'] == image.key):
-                toRet.append(image)
-
-    return toRet
 
 @app.route('/user/AddForSale', methods=['GET', 'POST'])
 def addForSale():
