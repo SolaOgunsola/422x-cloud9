be423fa7fd0340d6ee119de46bd71069b29a910b
epask <ecpaskey@iastate.edu>
2022-04-25 17:35:07 -0500

    fix login redirect

--- cloud_flask_app/app/views.py
+++ cloud_flask_app/app/views.py
@@ -24,8 +24,8 @@ def index():
                 environ.update(USERNAME=username)
                 environ.update(LOGIN='1')
                 print(environ.get('USERNAME'))
-                return redirect("user/dashboard")    
-    return render_template("public/index.html")
+                return redirect("/")    
+    return render_template("public/login.html")
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
