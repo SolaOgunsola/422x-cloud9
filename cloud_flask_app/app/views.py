4a32b0bf34e65d1ef0e462c47b80993a2bdf6d8f
David Imhoff <dbimhoff@iastate.edu>
2022-04-28 16:49:46 -0500

    removed buttn when logged in

--- cloud_flask_app/app/views.py
+++ cloud_flask_app/app/views.py
@@ -24,7 +24,7 @@ def index():
                 environ.update(USERNAME=username)
                 environ.update(LOGIN='1')
                 print(environ.get('USERNAME'))
-                return redirect("/")    
+                return render_template('public/index.html', loggedIn = environ.get("LOGIN"), name = username)  
     return render_template("public/login.html")
 
 @app.route("/register", methods=['GET', 'POST'])
